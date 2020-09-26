ed909bdaa6ff52c10cbe42fea1820988eeaaef18
daidenghui <3276336032@qq.com>
2020-09-26 20:08:24 +0800

    获取数据库的内容

--- 01 通过api连接到数据库/01 通过api连接到数据库.cpp
+++ 01 通过api连接到数据库/01 通过api连接到数据库.cpp
@@ -7,8 +7,9 @@
 #define _DBNAME_ "mydb1"
 
 using namespace std;
-int main()
-{
+
+// 插入数据到数据库
+void inster_data() {
 	//1. init 
 	MYSQL* mysql = mysql_init(NULL);
 	if (mysql == NULL) {
@@ -25,14 +26,14 @@ int main()
 	cout << "hello mysql!" << endl;
 
 	// 设置字符格式
-	char rSql[256] = {0};
+	char rSql[256] = { 0 };
 	strcpy_s(rSql, "set names gbk");
 	if (mysql_query(mysql, rSql) != 0) {
-		cout << "1插入错误" << endl;
+		cout << "设置字符格式失败" << endl;
 		exit(1);
 	}
 	// 插入数据
-	strcpy_s(rSql,"INSERT INTO aa VALUE(10,'代')");
+	strcpy_s(rSql, "INSERT INTO emp VALUE(5,'代',1,'2018-08-27',20000,'2020-07-05','一个大傻逼')");
 	if (mysql_query(mysql, rSql) != 0) {
 		cout << "2插入错误" << endl;
 		exit(1);
@@ -41,5 +42,61 @@ int main()
 
 	//3. close
 	mysql_close(mysql);
+}
+
+// 得到数据库的数据
+void get_data() {
+	//1. init 
+	MYSQL* mysql = mysql_init(NULL);
+	if (mysql == NULL) {
+		cout << "init err" << endl;
+		exit(1);
+	}
+	//2. real_connect
+	mysql = mysql_real_connect(mysql, _HOST_, _USER_, _PASSWD_, _DBNAME_, 0, NULL, 0);
+
+	if (mysql == NULL) {
+		cout << "connect err" << endl;
+		exit(1);
+	}
+	cout << "hello mysql!" << endl;
+
+
+	// 命令
+	char rSql[256] = { 0 };
+	// 设置编码格式
+	strcpy_s(rSql, "set names gbk");
+	if (mysql_query(mysql, rSql) != 0) {
+		cout << "设置字符格式失败" << endl;
+		exit(1);
+	}
+	strcpy_s(rSql, "select * from emp");
+	if (mysql_query(mysql, rSql) != 0) {
+		cout << "查询失败" << endl;
+		exit(1);
+	}
+	// 取回结果集
+	MYSQL_RES * result = mysql_store_result(mysql);
+	MYSQL_ROW row;
+	int i = 0;
+	if (result != NULL) {
+		// 需要打印结果集
+		// 循环取一行
+		while ((row = mysql_fetch_row(result)) != NULL ) {
+			for (i = 0; i < 7; i++) {
+				cout << row[i] << " ";
+			}
+			cout << endl;
+		}
+		// 释放结果集
+		mysql_free_result(result);
+	}
+	//3. close
+	mysql_close(mysql);
+}
+int main()
+{
+	//inster_data();
+	get_data();
 	return 0;
 }
